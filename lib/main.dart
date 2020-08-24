a21493b0183d70af4fe5040fb50ff6e677a54710
nocaed <codenamedeacon@outlook.com>
2020-08-24 16:56:26 -0400

    added state variables to input form

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:bmitracker/bmi.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+/// Main app widget.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,45 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Center(child: Text('Hello World'),),
+    );
+  }
+}
+
+/// The input form for BMI.
+class BMIInputForm extends StatefulWidget {
+  @override
+  createState() => _BMIInputFormState();
+}
+
+// The state of the input form.
+class _BMIInputFormState extends State<BMIInputForm> {
+  // state variables
+  // form key that manages input state
+  final _formKey = GlobalKey<FormState>();
+  // controller for weight input
+  final _weightController = TextEditingController();
+  // controller for height input
+  final _heightController = TextEditingController();
+
+  // the current bmi
+  double _currentBMI;
+  // the current weight
+  double _currentWeight;
+  // the currently selected unit of measurement
+  BMIUnit _selectedUnit;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentBMI = 0;
+    _currentWeight = 0;
+    _selectedUnit = BMIUnit.customary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
     );
   }
 }
