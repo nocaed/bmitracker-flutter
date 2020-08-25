981e607cee231a69650008aa7b4967f44cd05a1b
nocaed <codenamedeacon@outlook.com>
2020-08-25 00:30:59 -0400

    added creation and insertion to database query system

--- lib/db.dart
+++ lib/db.dart
@@ -2,8 +2,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'dart:async';
 
-const String databaseName = 'test.db';
-
+/// Data model for querying bmi database.
 class BMILog {
   int _id;
   double _bmi;
@@ -21,5 +20,33 @@ class BMILog {
       'bmi': _bmi,
       'date': _date
     };
+  }
+}
+
+class BMIDatabaseQuerySystem {
+  static final BMIDatabaseQuerySystem _singleton = BMIDatabaseQuerySystem._internal();
+  static const String _databaseName = 'test.db';
+  Database _database;
+
+  factory BMIDatabaseQuerySystem() => _singleton;
+
+  BMIDatabaseQuerySystem._internal() {
+    _startDb();
+  }
+
+  void _startDb() async {
+    _database = await openDatabase(
+      join(await getDatabasesPath(), _databaseName),
+      onCreate: (db, version) => db.execute('CREATE TABLE bmilogs(id INTEGER PRIMARY KEY, bmi REAL, date TEXT)'),
+      version: 1
+    );
+  }
+
+  Future<void> insertBMI(BMILog log) async {
+    await _database.insert(
+      'bmilogs',
+      log.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace
+    );
   }
 }
