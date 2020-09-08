5927d168ccb16578a7d317a2743458c232fc96a6
nocaed <codenamedeacon@outlook.com>
2020-09-08 19:00:10 -0400

    refactored into stateful widget

--- lib/log_screen.dart
+++ lib/log_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class LogScreen extends StatelessWidget {
+class LogScreen extends StatefulWidget {
+  @override
+  createState() => _LogScreenState();
+}
+
+class _LogScreenState extends State<LogScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
